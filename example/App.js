833c753d9d9359a222c73c3cc8e22a7c0bfa5e48
Jérémy Carbonne <jeremycarbonne@gmail.com>
2017-09-03 19:40:47 +0200

    Export css, it's possible to render a different component for each node

--- example/App.js
+++ example/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import {Yellow, Red, Dark} from './Block'
 import './App.css';
-import Minimap from 'react-minimap';
+import Minimap, {Child as ChildComponent} from 'react-minimap';
+import 'react-minimap.css'
 
 
+const YELLOW = 'yellow'
+const RED    = 'red'
+const DARK   = 'dark'
 
+const HEX = {
+  [YELLOW]: 'FFAB00', 
+  [RED]   : 'DD2C00',
+  [DARK]  : '212121',
+}
 
 class App extends Component {
 
@@ -14,34 +23,70 @@ class App extends Component {
     random3: {left: '350px', top: '50px'},
   }
 
+  random(max) {
+    return Math.round(Math.random() * max)
+  }
+
   componentDidMount() {
+
     setInterval(() => {
       this.setState({
         ...this.state,
-        random1: {left: Math.round(Math.random() * 3000), top: Math.round(Math.random() * 100)},
-        random2: {left: Math.round(Math.random() * 3000), top: Math.round(Math.random() * 100)},
-        random3: {left: Math.round(Math.random() * 3000), top: Math.round(Math.random() * 100)},
+        random1: {left: this.random(3000), top: this.random(100)},
+        random2: {left: this.random(3000), top: this.random(100)},
+        random3: {left: this.random(3000), top: this.random(100)},
       })
     }, 2000)
   }
 
+  renderChild({ width, height, left, top, node }) {
+
+    const classNames = [YELLOW, RED, DARK]
+    let classNameFound = null
+
+    node.classList.forEach(className => {
+      if (classNames.includes(className))
+      {
+        classNameFound = className
+        return false
+      }
+    })
+
+    if (classNameFound === null)
+      return <ChildComponent {...{width, height, left, top}} />
+
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          width,
+          height,
+          left,
+          top,
+          backgroundColor: '#' + HEX[classNameFound],
+        }}
+      />
+    )
+  }
+
   render() {
     const {random1, random2, random3} = this.state
+
     return (
       <div className="App">
         <div className="nav-bar">
           Nav bar
         </div>
-        <div id="container" style={{height: "calc(100vh - 60px)", position:"absolute", width: "100vw", backgroundColor: "gainsboro"}}>
+        <div className="container">
 
-          <Minimap selector=".box">
+          <Minimap selector=".box" childComponent={this.renderChild.bind(this)}>
             <Dark />
-            <Yellow style={{position:'relative', ...random1}}/>
-            <Red style={{position:'relative', width: "200px", left: '4000px', top: '100px'}}/>
-            <Yellow style={{position:'relative',...random2}} />
-            <Dark style={{position:'relative', ...random3}} />
-            <Yellow style={{position:'relative', width: "200px", left: '2000px'}} />
-            <Dark style={{position:'relative', width: "200px", left: '2000px', marginTop: "30px" }}/>
+            <Yellow className="pos-rlt" style={random1}/>
+            <Red className="pos-rlt" style={{width: "200px", left: '4000px', top: '100px'}}/>
+            <Yellow className="pos-rlt" style={random2} />
+            <Dark className="pos-rlt" style={random3} />
+            <Yellow className="pos-rlt" style={{width: "200px", left: '2000px'}} />
+            <Dark className="pos-rlt" style={{width: "200px", left: '2000px', marginTop: "30px" }}/>
 
             <Yellow />
             <Dark />
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export default Minimap from './react-minimap'
+export {default as Child} from './components/Child'
