607c4f0ae1500142394ab654af1322b5665b0df0
Jérémy CARBONNE <jeremycarbonne@gmail.com>
2018-05-16 14:29:44 +0200

    0.8.1-alpha-3 (#13)
    Split the main container in two containers (main and scroll container)

--- src/react-minimap.js
+++ src/react-minimap.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import _ from 'lodash';
+import React from 'react'
+import _ from 'lodash'
 import PropTypes from 'prop-types'
 import Child from './components/Child'
 import './react-minimap.css'
@@ -8,26 +8,26 @@ export class Minimap extends React.Component {
   static propTypes = {
     className: PropTypes.string,
     selector: PropTypes.string.isRequired,
-    width: PropTypes.number, /** in pixel */
-    height: PropTypes.number, /** in pixel */
+    width: PropTypes.number /** in pixel */,
+    height: PropTypes.number /** in pixel */,
     keepAspectRatio: PropTypes.bool,
     childComponent: PropTypes.any,
     onMountCenterOnX: PropTypes.bool,
     onMountCenterOnY: PropTypes.bool,
-  };
+  }
 
   static defaultProps = {
-    className: "",
+    className: '',
     width: 200,
     height: 200,
     keepAspectRatio: false,
     childComponent: Child,
     onMountCenterOnX: false,
     onMountCenterOnY: false,
-  };
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.down = this.down.bind(this)
     this.move = this.move.bind(this)
     this.synchronize = this.synchronize.bind(this)
@@ -41,21 +41,26 @@ export class Minimap extends React.Component {
       viewport: null,
       width: props.width,
       height: props.height,
-    };
+    }
 
     this.downState = false
     this.initState = false
   }
-  
+
   componentDidMount() {
-    const {onMountCenterOnX, onMountCenterOnY} = this.props
-    setTimeout(() => this.synchronize({centerOnX: onMountCenterOnX, centerOnY:onMountCenterOnY}));
-    window.addEventListener( "resize", this.resize);
+    const { onMountCenterOnX, onMountCenterOnY } = this.props
+    setTimeout(() =>
+      this.synchronize({
+        centerOnX: onMountCenterOnX,
+        centerOnY: onMountCenterOnY,
+      }),
+    )
+    window.addEventListener('resize', this.resize)
     this.init()
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.resize)
+    window.removeEventListener('resize', this.resize)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -81,188 +86,184 @@ export class Minimap extends React.Component {
     const {scrollWidth, scrollHeight, scrollTop, scrollLeft} = this.source;
     const sourceRect = this.source.getBoundingClientRect();
 
-    let {width, height} = this.props
+    let { width, height } = this.props
 
+    let ratioX = width / scrollWidth
+    let ratioY = height / scrollHeight
 
-    let ratioX = width / scrollWidth;
-    let ratioY = height / scrollHeight;
-    
     if (keepAspectRatio) {
       if (ratioX < ratioY) {
         ratioY = ratioX
-        height = Math.round( scrollHeight / (scrollWidth / width) )
+        height = Math.round(scrollHeight / (scrollWidth / width))
       } else {
         ratioX = ratioY
-        width = Math.round( scrollWidth / (scrollHeight / height) )
+        width = Math.round(scrollWidth / (scrollHeight / height))
       }
     }
 
-    const nodes = this.ref.querySelectorAll(this.props.selector)
+    const nodes = this.source.querySelectorAll(this.props.selector)
     this.setState({
       ...this.state,
       height,
       width,
       children: _.map(nodes, (node, key) => {
-        const {width, height, left, top} = node.getBoundingClientRect()
+        const { width, height, left, top } = node.getBoundingClientRect()
 
-        const wM = width * ratioX;
-        const hM = height * ratioY;
-        const xM = (left + scrollLeft - sourceRect.left) * ratioX;
-        const yM = (top + scrollTop - sourceRect.top) * ratioY;
+        const wM = width * ratioX
+        const hM = height * ratioY
+        const xM = (left + scrollLeft - sourceRect.left) * ratioX
+        const yM = (top + scrollTop - sourceRect.top) * ratioY
 
         return (
           <ChildComponent
             key={key}
-            width={Math.round( wM )}
-            height={Math.round( hM )}
-            left={Math.round( xM )}
-            top={Math.round( yM )}
+            width={Math.round(wM)}
+            height={Math.round(hM)}
+            left={Math.round(xM)}
+            top={Math.round(yM)}
             node={node}
           />
         )
-      })
+      }),
     })
   }
 
-  down( e ) {
+  down(e) {
     const pos = this.minimap.getBoundingClientRect()
 
-    this.x = Math.round( pos.left + this.l + this.w / 2 );
-    this.y = Math.round( pos.top + this.t + this.h / 2 );
+    this.x = Math.round(pos.left + this.l + this.w / 2)
+    this.y = Math.round(pos.top + this.t + this.h / 2)
 
     this.downState = true
-    this.move( e );
+    this.move(e)
   }
 
   up() {
     this.downState = false
   }
 
-  move( e ) {
-    if (this.downState === false)
-      return
+  move(e) {
+    if (this.downState === false) return
 
-    const {width, height} = this.state
-    let event;
+    const { width, height } = this.state
+    let event
 
-    e.preventDefault();
-    if ( e.type.match( /touch/ ) ) {
-      if ( e.touches.length > 1 ) {
-        return;
+    e.preventDefault()
+    if (e.type.match(/touch/)) {
+      if (e.touches.length > 1) {
+        return
       }
-      event = e.touches[ 0 ];
+      event = e.touches[0]
     } else {
-      event = e;
+      event = e
     }
 
-    let dx = event.clientX - this.x;
-    let dy = event.clientY - this.y;
-    if ( this.l + dx < 0 ) {
-      dx = -this.l;
+    let dx = event.clientX - this.x
+    let dy = event.clientY - this.y
+    if (this.l + dx < 0) {
+      dx = -this.l
     }
-    if ( this.t + dy < 0 ) {
-      dy = -this.t;
+    if (this.t + dy < 0) {
+      dy = -this.t
     }
-    if ( this.l + this.w + dx > width ) {
-      dx = width - this.l - this.w;
+    if (this.l + this.w + dx > width) {
+      dx = width - this.l - this.w
     }
-    if ( this.t + this.h + dy > height ) {
-      dy = height - this.t - this.h;
+    if (this.t + this.h + dy > height) {
+      dy = height - this.t - this.h
     }
 
-    this.x += dx;
-    this.y += dy;
+    this.x += dx
+    this.y += dy
 
-    this.l += dx;
-    this.t += dy;
+    this.l += dx
+    this.t += dy
 
-    // Sanity checks: 
-    this.l = (this.l < 0 ? 0 : this.l)
-    this.t = (this.t < 0 ? 0 : this.t)
+    // Sanity checks:
+    this.l = this.l < 0 ? 0 : this.l
+    this.t = this.t < 0 ? 0 : this.t
 
-    const coefX = width / this.source.scrollWidth;
-    const coefY = height / this.source.scrollHeight;
-    const left = this.l / coefX;
-    const top = this.t / coefY;
+    const coefX = width / this.source.scrollWidth
+    const coefY = height / this.source.scrollHeight
+    const left = this.l / coefX
+    const top = this.t / coefY
 
-    
-    this.source.scrollLeft = Math.round( left );
-    this.source.scrollTop = Math.round( top );
-    this.redraw();
+    this.source.scrollLeft = Math.round(left)
+    this.source.scrollTop = Math.round(top)
+    this.redraw()
   }
 
   synchronize(options) {
-    const {width, height} = this.state
+    const { width, height } = this.state
 
     const rect = this.source.getBoundingClientRect()
 
-    const dims = [ rect.width, rect.height ];
-    const scroll = [ this.source.scrollLeft, this.source.scrollTop ];
-    const scaleX = width / this.source.scrollWidth;
-    const scaleY = height / this.source.scrollHeight;
+    const dims = [rect.width, rect.height]
+    const scroll = [this.source.scrollLeft, this.source.scrollTop]
+    const scaleX = width / this.source.scrollWidth
+    const scaleY = height / this.source.scrollHeight
 
-    const lW = dims[ 0 ] * scaleX;
-    const lH = dims[ 1 ] * scaleY;
-    const lX = scroll[ 0 ] * scaleX;
-    const lY = scroll[ 1 ] * scaleY;
+    const lW = dims[0] * scaleX
+    const lH = dims[1] * scaleY
+    const lX = scroll[0] * scaleX
+    const lY = scroll[1] * scaleY
 
-    // Ternary operation includes sanity check 
-    this.w = (Math.round( lW ) > this.state.width) ? this.state.width : Math.round( lW );
-    this.h = (Math.round( lH ) > this.state.height) ? this.state.height : Math.round( lH );
-    this.l = Math.round( lX );
-    this.t = Math.round( lY );
+    // Ternary operation includes sanity check
+    this.w = Math.round(lW) > this.state.width
+      ? this.state.width
+      : Math.round(lW)
+    this.h = Math.round(lH) > this.state.height
+      ? this.state.height
+      : Math.round(lH)
+    this.l = Math.round(lX)
+    this.t = Math.round(lY)
 
     if (options !== undefined) {
       if (options.centerOnX === true) {
-        this.source.scrollLeft  = this.source.scrollWidth / 2 - dims[ 0 ]  / 2
+        this.source.scrollLeft = this.source.scrollWidth / 2 - dims[0] / 2
       }
 
       if (options.centerOnY === true) {
-        this.source.scrollTop  = this.source.scrollHeight / 2 - dims[ 1 ]  / 2
+        this.source.scrollTop = this.source.scrollHeight / 2 - dims[1] / 2
       }
     }
 
-    this.redraw();
+    this.redraw()
   }
 
   redraw() {
     this.setState({
-      ...this.state, 
+      ...this.state,
       viewport: (
-        <div 
-          className="minimap-viewport" 
+        <div
+          className="minimap-viewport"
           style={{
-            width : this.w,
-            height : this.h,
-            left : this.l,
-            top : this.t      
+            width: this.w,
+            height: this.h,
+            left: this.l,
+            top: this.t,
           }}
         />
-      )
+      ),
     })
   }
 
-
   render() {
-    const {width, height} = this.state
+    const { width, height } = this.state
 
-    return (  
-      <div 
-        className={"minimap-container " + this.props.className}
-        onScroll={this.synchronize} 
-        ref={(source) => {this.source = source;}}
-      >
-        <div 
+    return (
+      <div className={'minimap-container ' + this.props.className}>
+        <div
           className="minimap"
-          style={{            
-            width: `${width}px`, 
+          style={{
+            width: `${width}px`,
             height: `${height}px`,
           }}
-          
-          ref={(minimap) => { this.minimap = minimap; }} 
-
-          onMouseDown={this.down} 
-          onTouchStart={this.down} 
+          ref={minimap => {
+            this.minimap = minimap
+          }}
+          onMouseDown={this.down}
+          onTouchStart={this.down}
           onTouchMove={this.move}
           onMouseMove={this.move}
           onTouchEnd={this.up}
@@ -272,11 +273,17 @@ export class Minimap extends React.Component {
           {this.state.children}
         </div>
 
-        <div ref={(container) => { this.ref = container; }}>
+        <div
+          className={'minimap-container-scroll'}
+          onScroll={this.synchronize}
+          ref={container => {
+            this.source = container
+          }}
+        >
           {this.props.children}
         </div>
       </div>
-    );
+    )
   }
 }
 
